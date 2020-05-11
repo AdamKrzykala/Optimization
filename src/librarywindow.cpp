c81c50576ee66d6e4addd6db69004cdc4f4f84b9
adam20619 <adamkrzykala97@gmail.com>
2020-05-11 12:47:11 +0200

    Plotting finished

--- src/librarywindow.cpp
+++ src/librarywindow.cpp
@@ -67,9 +67,8 @@ void LibraryWindow::addFunctions(QListWidget *temp_list)
     temp_list->item(2)->setBackground(Qt::lightGray);
 
     temp_list->addItem("[Funkcja Goldensteina-Price'a]\n"
-                       "(1+(x2+x2+1)^2*(19-14*x1+3*x1^2-14x2+6*x1*x2"
-                       "+3*x2^2))*(30+(2*x1+(2*x1-3*x2)^2*(18-32*x1+12*x1^2+48*x2"
-                       "-36*x1*x2+27*27*x2^2))");
+                       "(1+(x1+x2+1)^2*(19-14*x1+3*x1^2-14*x2+6*x1*x2+3*x2^2))*(30+(2*x1-3*x2)^2*(18-32*x1+12*x1^2+48*x2-36*x1*x2+27*x2^2))");
+
     temp_list->addItem("[Funkcja celu szczególnie przeznaczona do testowaniaalgorytmów genetycznych]\n"
                        "exp(-2*log(2)*(x-0.08)^2/0.854^2)*sin(5*pi*(x^0.75-0.05)^6)");
     temp_list->item(4)->setBackground(Qt::lightGray);
--- src/nsgaalgorithm.cpp
+++ src/nsgaalgorithm.cpp
@@ -14,9 +14,16 @@ NSGAalgorithm::NSGAalgorithm(Borders borders,
     this->function1 = new FunctionParser(functions[1]);
     this->function2 = new FunctionParser(functions[2]);
     this->genetic_functions = new Allele(params);
+    QVector<QPointF> temp = QVector<QPointF>(this->_params["Lp"],QPointF(0,0));
+    this->history = QVector<QVector<QPointF>>(this->_params["Lg"],temp);
     //Initial Population Creating
     this->_parentPopulation = this->genetic_functions->initPopulation(this->_borders);
-    NSGA_MainLoop();
+    this->chart = new QChart();
+    this->chartView = new QChartView(this->chart);
+    this->chartView->setSizePolicy(QSizePolicy::Expanding,QSizePolicy::Minimum);
+    this->chartView->setMinimumHeight(600);
+    this->chartView->setVisible(false);
+    ui->verticalLayout_2->addWidget(this->chartView,0,0);
 }
 
 NSGAalgorithm::~NSGAalgorithm()
@@ -45,35 +52,84 @@ inline double my_clock(void) {
 void NSGAalgorithm::NSGA_MainLoop(void)
 {
     int do_iter = this->_params["Lg"];
+    ui->progressBar->setValue(0);
     double start_time, end_time, duration;
     start_time = my_clock();
     while(do_iter)
     {
     //Main Process
-        qDebug() << "Iteration: " << this->_params["Lg"] - do_iter + 1;
+        //qDebug() << "Iteration: " << this->_params["Lg"] - do_iter + 1;
         //Creating offspring population
-        qDebug() << "-----Creating offspring population";
+        //qDebug() << "-----Creating offspring population";
         this->_offspringPopulation = this->genetic_functions->offspringPopulation(this->_parentPopulation,
                                                                                   this->_borders);
         //Concatenating parent population and offspring population
-        qDebug() << "-----Concatenating parent and offspring population";
+        //qDebug() << "-----Concatenating parent and offspring population";
         Population processPopulation = this->ConcatenatePopulation(this->_parentPopulation,this->_offspringPopulation);
         //Creating fronted population
-        qDebug() << "-----Creating fronts";
+        //qDebug() << "-----Creating fronts";
         Population frontedPopulation = this->genetic_functions->frontedPopulation(processPopulation, *this->function1, *this->function2, this->_borders);
         //Calculate crowding in each front
         this->_parentPopulation.clear();
         this->_offspringPopulation.clear();
-        qDebug() << "-----Calcualte crowding distances";
+        //qDebug() << "-----Calcualte crowding distances";
         this->_parentPopulation = this->genetic_functions->calculateCrowding(frontedPopulation, *this->function1, *this->function2);
+        int it = this->_params["Lg"] - do_iter;
+        for(int i = 0; i < this->_parentPopulation.size(); i++){
+            QPointF temp = QPointF(this->function1->getValue(this->_parentPopulation.at(i).first),this->function2->getValue(this->_parentPopulation.at(i).first));
+            this->history[it][i] = temp;
+        }
         --do_iter;
+        ui->progressBar->setValue((int)(this->_params["Lg"]-do_iter)*100/this->_params["Lg"]);
     }
     end_time = my_clock();
     duration = end_time - start_time;
+    ui->timeLine->setText(QString::number(duration)+" sek");
+    ui->iterationScreen->setText(QString("%1").arg(this->_params["Lg"]));
+    setScatterPlot(this->_params["Lg"]-1);
+}
 
-    qDebug() << "Operation took "<< duration << " seconds";
-//    qDebug() <<"x:"<< _parentPopulation;
-    qDebug() <<"f1 value: "<<this->function1->getValue(_parentPopulation.first().first);
-    qDebug() <<"f2 value: "<<this->function2->getValue(_parentPopulation.first().first);
+void NSGAalgorithm::setScatterPlot(int it)
+{
+    this->chartView->chart()->removeAllSeries();
+    this->chartView->setVisible(true);
+    QScatterSeries *series = new QScatterSeries();
+    series->setMarkerShape(QScatterSeries::MarkerShapeCircle);
+    series->setMarkerSize(12);
+    for (int i = 0; i < this->_parentPopulation.size(); i++){
+        *series << this->history.at(it).at(i);
+    }
+    this->chartView->chart()->addSeries(series);
+    this->chartView->chart()->createDefaultAxes();
+}
 
+void NSGAalgorithm::on_pushButton_clicked()
+{
+    NSGA_MainLoop();
+}
+
+void NSGAalgorithm::on_decrementation_clicked()
+{
+    int current = ui->iterationScreen->text().toInt();
+    if ((current-1) > 0){
+        ui->iterationScreen->setText(QString("%1").arg(current-1));
+    }
+}
+
+void NSGAalgorithm::on_iterationScreen_textChanged(const QString &arg1)
+{
+
+    if (!(ui->iterationScreen->text() == "") &&
+         (ui->iterationScreen->text().toInt() >= 1) &&
+         (ui->iterationScreen->text().toInt() <= this->history.size())){
+        this->setScatterPlot(ui->iterationScreen->text().toInt()-1);
+    }
+}
+
+void NSGAalgorithm::on_incrementation_clicked()
+{
+    int current = ui->iterationScreen->text().toInt();
+    if ((current) < this->history.size()){
+        ui->iterationScreen->setText(QString("%1").arg(current+1));
+    }
 }
